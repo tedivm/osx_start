c2f7dfae2f71795b4d602ce5e9a52b8f4e4dcdf0
Robert Hafner <tedivm@tedivm.com>
2014-10-15 00:50:57 -0700

    Created system setup script
    This script installs build tools, packages and applications using the
    homebrew package manager (which is also installs if needed).

---
+++ startup.sh
@@ -0,0 +1,160 @@
+#!/bin/sh
+
+# OSX Start v1.0.0
+# (c)2014 Robert Hafner <tedivm@tedivm.com>
+# MIT License (see LICENSE file)
+# https://github.com/tedivm/osx_start
+
+
+# Apps to install with Caskroom.
+# Installed to /opt/homebrew-cask/Caskroom/ and linked to /Applications.
+apps=(
+  adium
+  apikitchen
+  arduino
+  atom
+  caffeine
+  crashplan
+  cyberduck
+  disk-inventory-x
+  dropbox
+  firefox
+  flash
+  flux
+  github
+  google-chrome
+  hermes
+  iterm2
+  kindle
+  lastpass-universal
+  mou
+  omnigraffle
+  omnioutliner
+  phpstorm
+  qlmarkdown
+  qlprettypatch
+  qlstephen
+  quicklook-json
+  send-to-kindle
+  sequel-pro
+  sketch
+  skype
+  slack
+  spotify
+  sublime-text3
+  the-unarchiver
+  textmate
+  tower
+  transmission
+  transmit
+  vagrant
+  versions
+  virtualbox
+)
+
+
+# Build tools- install these first so other tools are built with them.
+build=(
+  autoconf
+  bash
+  coreutils
+  findutils
+  homebrew/dupes/grep
+)
+
+
+# CLI applications to install with Brew.
+binaries=(
+  ack
+  android-sdk
+  cmake
+  curl
+  ffmpeg
+  git
+  graphicsmagick
+  hhvm
+  hub
+  nmap
+  node
+  pcre
+  python
+  rename
+  sshfs
+  trash
+  tree
+  webkit2png
+  wget
+  zopfli
+)
+
+
+# Fonts to install with Homebrew Fonts
+fonts=(
+  font-clear-sans
+  font-inconsolata
+  font-m-plus
+  font-roboto
+)
+
+
+# Trigger OSX CLI Install package if needed, otherwise do nothing.
+echo "Checking for Xcode Command Line Tools"
+gcc --version > /dev/null
+
+
+# Check for Homebrew, install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+
+# Add the base stuff
+echo "Installing base build packages..."
+brew tap homebrew/dupes
+brew install ${build[@]}
+
+
+# Update homebrew recipes
+echo "Upgrading homebrew and installed packages..."
+brew update
+brew upgrade
+
+
+# Add override and backup paths to override system binaries and add new applications, respectively.
+export PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH:/usr/local/opt
+buildPathAddition="export PATH=\$(brew --prefix coreutils)/libexec/gnubin:$PATH:/usr/local/opt"
+if grep -q "$buildPathAddition" ~/.profile
+then
+    echo "Build path already saved..."
+else
+    echo "Saving build path to ~/.profile..."
+    echo  $buildPathInfo >> ~/.profile
+    echo "export ANDROID_HOME=/usr/local/opt/android-sdk" >> ~/.profile
+
+fi
+
+
+# Add base packages
+echo "Installing binaries..."
+brew install ${binaries[@]}
+brew install caskroom/cask/brew-cask
+
+
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+echo "installing apps..."
+brew cask install --appdir="/Applications" ${apps[@]}
+
+
+# Install fonts
+echo "Installing fonts..."
+brew tap caskroom/fonts
+brew cask install ${fonts[@]}
+
+
+# Right now cleanup
+echo "Cleaning up..."
+brew cleanup
+
+echo "All done!"
